2a20d595e024f346fa2d6fb24d7bbc43b82d29be
Vitech <50809536+Visels@users.noreply.github.com>
2022-02-16 23:31:49 +0300

    Area update

--- java/com/thecodevillage/day9/MyArea.java
+++ java/com/thecodevillage/day9/MyArea.java
@@ -3,6 +3,8 @@ package com.thecodevillage.day9;
 import java.text.DecimalFormat;
 import java.util.Scanner;
 
+
+// This is a function that calculates the area of a region
 public class MyArea {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
